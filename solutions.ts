35c4096467d21919c132b17efd512300ffd89173
Taher027 <abutaherh27@gmail.com>
2026-05-06 23:10:20 +0600

    solved six question

--- solutions.ts
+++ solutions.ts
@@ -3,17 +3,17 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
   return numbers.filter((num) => num % 2 === 0);
 };
 
-const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]);
+const evenNumbers = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]);
 
-// sollution of probelm 2 ;
 const reverseString = (input: string): string => {
-  const reversedString = input.split("").reverse().join("");
-  return reversedString;
+  const newReversedstring = input.split("").reverse().join("");
+  return `"${newReversedstring}"`;
 };
 
 const reversedString = reverseString("Hello");
 
 // sollution of probelm 3 ;
+
 type StringOrNumber = string | number;
 
 const checkType = (input: StringOrNumber): string => {
@@ -29,3 +29,51 @@ const type1 = checkType("Hello");
 const type2 = checkType(42);
 
 // sollution of probelm 4;
+const getProperty = <T extends object, K extends keyof T>(
+  obj: T,
+  key: K,
+): T[K] => {
+  return obj[key];
+};
+
+const userName = getProperty({ id: 1, name: "Taher", age: 30 }, "name");
+
+//sollution of problem 5
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+const toggleReadStatus = (book: Book): Book & { isRead: boolean } => {
+  return { ...book, isRead: true };
+};
+const myBook = toggleReadStatus({
+  title: "Ts",
+  author: "Taher",
+  publishedYear: 2026,
+});
+
+//sollution of problem 6
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `"Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}";`;
+  }
+}
+
+const student = new Student("Alice", 20, "A");
